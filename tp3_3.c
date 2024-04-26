47fab8c34049e872cd868feac37958bd15d364e4
Agustin <agochoc1@gmail.com>
2024-04-26 17:03:45 -0300

    ejercicio 5 terminado

--- tp3_3.c
+++ tp3_3.c
@@ -8,21 +8,29 @@ int main(){
 
     char **nombres;
     char buff[100];
+    int cantNombres;
+    printf("Ingrese el numero de nombres a ingresar:\n");
+    scanf("%d", &cantNombres);
 
-    nombres = (char**) malloc(sizeof(char*)*5);
+    nombres = (char**) malloc(sizeof(char*) * cantNombres);
 
-    printf("Ingrese 5 nombres:\n");
-    for (int i = 0; i < 5; i++)
+    printf("Ingrese %d nombres:\n", cantNombres);
+    for (int i = 0; i < cantNombres; i++)
     {
+        if (i == 0)
+        {
+            getchar();
+        }
         gets(buff);
         nombres[i] = (char*) malloc( sizeof(char) * strlen(buff));
         strcpy(nombres[i], buff);
         printf("Ingrese el siguiente nombre:\n");
     }
 
-    printf("\n---------------\n");
-    for (int j = 0; j < 5; j++)
+    printf("\n---------------------------\n");
+    for (int j = 0; j < cantNombres; j++)
     {
+        printf("-");
         puts(nombres[j]);
     }
     return 0;
